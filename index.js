19c5ad9c0d3bcd23341397219ff834be9315cbf9
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-04-07 10:32:14 -0600

    added base endpoint for testing

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-require('dotenv').config()
-
 const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 9000;
+
+server.get("/", (req, res) => {
+  res.send("Secret Recipes Hidden Here")
+})
+
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
